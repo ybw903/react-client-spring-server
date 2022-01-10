495190227aee1ae6c43d0727fbfe7f8b809b2545
ybw903 <conner903@naver.com>
2022-01-10 22:35:04 +0900

    client: post file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import './App.css';
+
+const baseURL = "http://localhost:8080"
 
 function App() {
   const [file, setFile] = useState<any>(null);
@@ -9,10 +12,33 @@ function App() {
       return;
     setFile(evt.target.files[0]);
   }
+
+  const onFileSubmit: React.FormEventHandler<HTMLFormElement> = async (evt) => {
+    evt.preventDefault();
+    const formData = new FormData();
+    if (file === null) {
+      console.log("file is Empty!");
+      return;
+    }
+    formData.append("file", file);
+    try {
+      const response = await axios({
+        method: "POST",
+        url: `${baseURL}/api/v1/upload/file`,
+        data: formData,
+        headers: {"Content-Type": "multipart/form-data"}
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div className="App">
-      <input type="file" onChange={onFileChange}/>
-      <button></button>
+      <form onSubmit={onFileSubmit}>
+        <input type="file" onChange={onFileChange}/>
+        <input type="submit" value="업로드"></input>
+      </form>
     </div>
   );
 }
