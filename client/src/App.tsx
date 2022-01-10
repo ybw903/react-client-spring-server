a3893aa88f33a957ffab670a6910801e6590da96
ybw903 <conner903@naver.com>
2022-01-10 22:11:39 +0900

    client: input file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [file, setFile] = useState<any>(null);
+
+  const onFileChange: React.ChangeEventHandler<HTMLInputElement> = (evt) => {
+    if (!evt.target.files)
+      return;
+    setFile(evt.target.files[0]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={onFileChange}/>
+      <button></button>
     </div>
   );
 }
